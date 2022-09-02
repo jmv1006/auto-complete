20c3f2d8713f3ce6bde9381e82a4965f971ce0d0
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-09-02 11:38:37 -0700

    working on dfs algorithm to find possible words based on user input

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import trie from './trie'
+import trie from "./trie";
 
-const input = document.getElementById('input')
+const input = document.getElementById("input");
 
 function onChange(e) {
-  const list = document.getElementById("ul")
-  list.innerHTML = ""
+  const list = document.getElementById("ul");
+  list.innerHTML = "";
 
   const input = e.target.value;
   const value = input.split(" ").pop();
 
-  const characters = trie.complete(value);
+  const characters = trie.auto_complete(value);
 
-  if(characters.length === 0) return
-
+  /*
   characters.forEach(character => {
     const listitem = document.createElement("li")
     listitem.innerHTML = value + character
     list.appendChild(listitem)
   })
+  */
 }
 
-input.addEventListener('input', onChange)
+input.addEventListener("input", onChange);
--- src/trie.js
+++ src/trie.js
@@ -1,52 +1,61 @@
-import words from './words';
+import words from "./words";
 
 class Trie {
-    constructor(root) {
-        this.root = root;
+  constructor(root) {
+    this.root = root;
+  }
+
+  insert(word) {
+    let node = this.root;
+    for (let char in word) {
+      if (!(word[char] in node.characters))
+        node.characters[word[char]] = new TrieNode(word[char]);
+      node = node.characters[word[char]];
+    }
+    node.isEndOfWord = true;
+  }
+
+  auto_complete(word) {
+    if (word.length < 1) return [];
+    let node = this.root;
+
+    //updating the current node to the last character of inputted string
+    for (let letter in word) {
+      if (word[letter] in node.characters) {
+        node = node.characters[word[letter]];
+      }
     }
 
-    insert(word) {
-        let node = this.root;
-        for (let char in word) {
-        if (!(word[char] in node.characters))
-            node.characters[word[char]] = new TrieNode(word[char]);
-        node = node.characters[word[char]];
-        }
-        node.isEndOfWord = true;
+    let possibleWords = [];
+
+    for (let character in node.characters) {
+      const possiblePath = this.find_end_path(node.characters[character]);
+      if (possiblePath) possibleWords.push(word.concat(possiblePath));
     }
 
-    complete(word) {
-        if(word.length < 1) return [];
-        let node = this.root
-        let result = [];
+    console.log(possibleWords);
 
-        //sets the current node to a node that matches the last character in word
-        for(let letter in word) {
-            if(word[letter] in node.characters) {
-                node = node.characters[word[letter]]
-            }
-        }
-        
-        for(let char in node.characters) {
-            if(node.characters[char].isEndOfWord) result.push(char)
-        }
+    return [];
+  }
 
-        return result;
-    }
+  find_end_path(node) {
+    if (node.isEndOfWord) return node.char;
+    //in this case, keep searching for an end of word
+  }
 }
 
 class TrieNode {
-    constructor(char) {
-        this.char = char;
-        this.characters = {};
-        this.isEndOfWord = false;
-    }
+  constructor(char) {
+    this.char = char;
+    this.characters = {};
+    this.isEndOfWord = false;
+  }
 }
 
 const root = new TrieNode("");
 
 const trie = new Trie(root);
 
-words.forEach(word => trie.insert(word))
+words.forEach((word) => trie.insert(word));
 
-export default trie
+export default trie;
